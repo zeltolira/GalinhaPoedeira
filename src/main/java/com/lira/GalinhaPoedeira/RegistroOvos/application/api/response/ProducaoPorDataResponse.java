443043fed207c29e17fbf47e986ee96c137b61e7
unknown <zelto@hotmail.com>
2024-07-10 22:14:30 -0300

    Deleta classe nao usada

--- src/main/java/com/lira/GalinhaPoedeira/RegistroOvos/application/api/response/ProducaoPorDataResponse.java
+++ src/main/java/com/lira/GalinhaPoedeira/RegistroOvos/application/api/response/ProducaoPorDataResponse.java
@@ -1,18 +1,33 @@
 package com.lira.GalinhaPoedeira.RegistroOvos.application.api.response;
 
+import com.lira.GalinhaPoedeira.Galinha.domain.Galinha;
 import lombok.Data;
 import lombok.ToString;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @ToString
 public class ProducaoPorDataResponse {
     private String nomeGalinha;
     private int quantidade;
-
+    private Galinha galinha;
+    private List<RegistroOvosResponse> registroOvos;
+    private Integer producaoDiaria;
     public ProducaoPorDataResponse(String nomeGalinha, int quantidade) {
         this.nomeGalinha = nomeGalinha;
         this.quantidade = quantidade;
+        this.registroOvos = galinha.getRegistroOvos().stream()
+                .map(RegistroOvosResponse::new)
+                .collect(Collectors.toList());
+        this.producaoDiaria = calcularSomaOvos();
+    }
 
+    private Integer calcularSomaOvos() {
+        return registroOvos.stream()
+                .mapToInt(RegistroOvosResponse::getQuantidade)
+                .sum();
     }
 }
 
